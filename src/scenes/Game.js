ac0bcc8db12cc83559088955d6af32ee6a7cc94f
Mihai Vasilache <mihai_vasilache@yahoo.com>
2020-12-16 20:29:16 +0200

    atelier7

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,19 +1,42 @@
-/// <reference path="../../typings/phaser.d.ts" />
+
 // @ts-check
 
 import Phaser from 'phaser';
+import Hero from '../entities/Hero.js';
 
 class Game extends Phaser.Scene {
   preload() {
     this.load.image('mage', 'assets/mage/mage.png');
+    this.load.spritesheet('idle-spritesheet', 'assets/mage/idle.png', { frameWidth: 171, frameHeight: 128 });
+    this.load.spritesheet('walk-spritesheet', 'assets/mage/walk.png', { frameWidth: 171, frameHeight: 128 });
   }
   create() {
-   this.hero= this.physics.add.sprite(400, 300, 'mage');
 
-this.hero.body.setCOLLIdeWorldBounds(true)
+
+    this.anims.create({
+      key: 'hero-idle',
+      frames: [
+        { frame: 0, key: 'mage', duration: 10000 },
+        ...this.anims.generateFrameNumbers('idle-spritesheet', {})
+      ],
+      frameRate: 6,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'hero-walk',
+      frames: this.anims.generateFrameNumbers("walk-spritesheet", {}),
+      frameRate: 6,
+      repeat: -1
+    });
+
+    let hero = new Hero(this, 400, 300);
+
+
 
 
 
   }
 }
+
 export default Game;
